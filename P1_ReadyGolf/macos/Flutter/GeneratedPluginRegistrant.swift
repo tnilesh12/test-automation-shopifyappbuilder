8b8aa0a4788d092f0fc84945110622e9583e27ca
Nilesh <nileshtiwari@itgeeks.com>
2024-10-09 11:16:47 +0530

    done

--- P1_ReadyGolf/macos/Flutter/GeneratedPluginRegistrant.swift
+++ P1_ReadyGolf/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import flutter_local_notifications
 import just_audio
 import package_info_plus
 import path_provider_foundation
+import screen_brightness_macos
 import share_plus
 import shared_preferences_foundation
 import sqflite
@@ -39,6 +40,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
--- P1_ReadyGolf/windows/flutter/generated_plugins.cmake
+++ P1_ReadyGolf/windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
   file_selector_windows
   firebase_core
+  screen_brightness_windows
   share_plus
   url_launcher_windows
 )
